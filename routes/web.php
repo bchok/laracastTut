b595468a41b5b9416cd785eab4db2b7c79c70c32
bchok <bchok91@gmail.com>
2017-10-24 14:26:19 -0400

    implemented layouts with posts (video 7)

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,9 @@
 Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}', 'TasksController@show');
 
+Route::get('/', 'PostController@index');
+
+Route::get('/posts/{post}', 'PostController@show');
+
 
 
